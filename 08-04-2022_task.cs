74b738626bca8fbf47be3c4adaf028dd7d769c63
NiTHin1410 <101178471+NiTHin1410@users.noreply.github.com>
2022-04-08 19:05:58 +0530

    Add files via upload

---
+++ 08-04-2022_task.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LynQ
+{
+    class _08_04_2022_task
+    {
+    static int countGoat(int eyes,int Legs)
+			{
+				int count = 0;
+                count = (Legs) - (eyes - 1);
+				count = count / 2;
+				return count;
+             }
+    public static void Main()
+			{
+				int eyes = 52, Legs = 80;
+				int Goat = countGoat(eyes, Legs);
+				Console.WriteLine("Goat = " +Goat);
+				Console.WriteLine("Ducks = " +(Goat - 2));
+			}
+		}
+	}
